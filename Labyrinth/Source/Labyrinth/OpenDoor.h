0aa13caacc62c90ed51a57bcc237118884918a30
masoncoram <mcoram777@yahoo.com>
2019-01-13 20:04:02 -0700

    Commit to Lecture 73 without ipch files

---
+++ Labyrinth/Source/Labyrinth/OpenDoor.h
@@ -0,0 +1,28 @@
+// Copyright Mason Coram and Brad Marx 2019
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "OpenDoor.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class LABYRINTH_API UOpenDoor : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UOpenDoor();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+		
+};
